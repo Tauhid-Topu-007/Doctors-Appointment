2a98cb1cdf51321d51b0aebb810a10fd061ea59d
tauhid-topu <t.topu021@gmail.com>
2025-08-29 19:04:56 +0600

    navbar,banner,footer,header,related doctors,speciality menu,top doctors,about,appointment,contact,doctors,home

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Header from '../components/Header'
+import SpecialityMenu from '../components/SpecialityMenu'
+import Banner from '../components/Banner'
+import TopDoctors from '../components/TopDoctors'
+
+
+const Home = () => {
+  return (
+    <div>
+        <Header/>
+        <SpecialityMenu/>
+        <TopDoctors/>
+        <Banner/>
+    </div>
+  )
+}
+
+export default Home
