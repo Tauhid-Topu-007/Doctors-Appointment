5eb1b9caa7f42dc2789dba2c8b8082de4e8d3a39
tauhid-topu <t.topu021@gmail.com>
2025-09-04 18:46:40 +0600

    admin controller

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import connectDB from './config/mongodb.js';
+import connectCloudinary from './config/cloudinary.js';
+import adminRouter from './routes/adminRoute.js';
+
+//app config
+const app=express();
+const port=process.env.PORT || 4000;
+connectDB();
+connectCloudinary();
+
+
+//middlewares
+app.use(cors());
+app.use(express.json());
+
+
+
+//api endpoint
+
+app.use('/api/admin',adminRouter)
+
+
+app.get('/',(req,res)=>{
+    res.send('Api working very well');
+})
+
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`);
+});
