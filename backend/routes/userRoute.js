6712df05210ba53474be7253823c948bfca0a0ed
tauhid-topu <t.topu021@gmail.com>
2025-09-10 20:39:29 +0600

    backend connection with frontend, doctorController, userController,userRoutes

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,17 +1,9 @@
-import express from 'express'
-import { registerUser, loginUser, getProfile, updateProfile, bookAppointment, listAppointment, cancelAppointment } from '../controllers/userController.js'
-import authUser from '../middleware/authUser.js'
-import upload from '../middleware/multer.js'
+import express from 'express';
+import {loginUser, registerUser} from '../controllers/userController.js';
 
-const userRouter = express.Router()
+const userRouter=express.Router();
 
-userRouter.post('/register',registerUser)
-userRouter.post('/login',loginUser)
+userRouter.post('/register',registerUser);
+userRouter.post('/login',loginUser);
 
-userRouter.get('/get-profile',authUser,getProfile)
-userRouter.post('/update-profile',upload.single('image'),authUser,updateProfile)
-userRouter.post('/book-appointment',authUser,bookAppointment)
-userRouter.get('/appointments',authUser,listAppointment)
-userRouter.post('/cancel-appointment',authUser,cancelAppointment)
-
-export default userRouter
+export default userRouter;
--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -1,18 +1,51 @@
 import { createContext, useEffect, useState } from "react";
 import axios from "axios"
 import {toast} from 'react-toastify'
-import { doctors } from "../assets/assets";
 
 
 export const AppContext = createContext()
 
 const AppContextProvider = (props) => {
 
-    const currencySymbol='$'
-    const value = {
-        doctors,
-        currencySymbol
+    const currencySymbol = '$'
+    const backendUrl = import.meta.env.VITE_BACKEND_URL
+
+    const [doctors,setDoctors] = useState([])
+    const [token,setToken] = useState(localStorage.getItem('token') ? localStorage.getItem('token') : false)
+
+    const [userData, setUserData] = useState(false)
+
+
+    const getDoctorsData = async () => {
+        try {
+            
+            const {data} = await axios.get(backendUrl + '/api/doctor/list')
+            if (data.success) {
+                setDoctors(data.doctors)
+            } else{
+                toast.error(data.message)
+            }
+
+        } catch (error) {
+            console.log(error)
+            toast.error(error.message)
+        }
     }
+
+
+    const value = {
+        doctors,getDoctorsData,
+        currencySymbol,
+        token,setToken,
+        backendUrl,
+        userData,setUserData,
+    }
+
+
+    useEffect(()=>{
+        getDoctorsData()
+    },[])
+
 
     return (
         <AppContext.Provider value={value}>
